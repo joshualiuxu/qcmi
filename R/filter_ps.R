68d94e8433c78b078b8790760486dba35c21620f
Joshua <519877651@qq.com>
2022-10-28 16:39:48 +0800

    Update filter_ps.R

--- R/filter_ps.R
+++ R/filter_ps.R
@@ -10,9 +10,9 @@
 
 
 filter_ps  = function(ps = ps,  occurrence.threshold=0 ,  abundance.threshold= 0 ){
-  ps1<- filter_taxa(ps,function(x)sum(x>0)>occurrence.threshold,TRUE)
-  ps2 <- prune_taxa(taxa_sums(ps1)>abundance.threshold,ps1)
-  filteredps<-rarefy_even_depth(ps2, sample.size = min(sample_sums(ps2)), rngseed = FALSE, replace = TRUE, trimOTUs = TRUE, verbose = TRUE)
-
-  return(filteredps)
+ps1<- filter_taxa(ps,function(x)sum(x>0)>occurrence.threshold,TRUE)
+ps2 <- prune_taxa(taxa_sums(ps1)>abundance.threshold,ps1)
+#filteredps<-rarefy_even_depth(ps2,  rngseed = FALSE, replace = TRUE, trimOTUs = TRUE, verbose = TRUE)
+filteredps=ps2
+return(filteredps)
 }
